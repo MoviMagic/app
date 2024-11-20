4e39e2453424df1b4f767f4f3d760a84f1c1be27
MoviMagic <store.rp04@gmail.com>
2024-11-20 00:11:27 -0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "273617900"
+    serviceWorkerVersion: "2559662716"
   }
 });
