18d75357d63b2d4aa59d15bf50332b4be24304cb
MoviMagic <store.rp04@gmail.com>
2025-04-13 01:00:27 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,14 +33,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a4be78bf24dee26af8a7e2bb95625550
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "b0465b34abc282647b82885a12b1db90",
+"flutter_bootstrap.js": "fff7fd979300381b531a08227e27ea63",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "926ebe64a6db0eacb1e7dd74f727f9b7",
 "/": "926ebe64a6db0eacb1e7dd74f727f9b7",
-"main.dart.js": "4f8df30a9b98be1be78036d386fdf5d7",
+"main.dart.js": "0d98d238ad7dff6912bb284eddd32ee5",
 "manifest.json": "203e2a96d528ce0939337ef2982ce0c8",
 "version.json": "2a769b0264d5f3f9961ffc729566cac8"};
 // The application shell files that are downloaded before a service worker can
