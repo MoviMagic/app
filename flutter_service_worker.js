e372a1e3fc84dc656fbb6e91967f1161196e472a
MoviMagic <store.rp04@gmail.com>
2025-07-16 05:02:34 -0400

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a4be78bf24dee26af8a7e2bb95625550
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "61754033ab135500450c0addd883c8a6",
+"flutter_bootstrap.js": "2c20714b0babc401eacd2fabf8ddd8e5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
